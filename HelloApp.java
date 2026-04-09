39fdbf7b2f3a3f231c265338131fd712b9c66b6f
PixelB0B <rajdeepwallangdey@gmail.com>
2026-04-09 22:16:00 +0530

    UC1: Display Hello World

---
+++ HelloApp.java
@@ -0,0 +1,10 @@
+/*
+ * UC1: Display "Hello World"
+ * Developer: Rajdeep
+ */
+
+public class HelloApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
